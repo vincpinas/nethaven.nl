7f00d2feaaa4059ee6490a118c0317dbeb808c9d
vincpinas <30472@ma-web.nl>
2022-07-17 19:13:32 +0200

    :blue_heart: about page begin

---
+++ web/src/Containers/AboutUs/AboutUs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div id='aboutUsContainer'>
+      <section id="whoAreWe"></section>
+      <section id="whatWeDo"></section>
+      <section id="whyWeDo"></section>
+      <section id="meetTheTeam"></section>
+    </div>
+  )
+}
+
+export default AboutUs
